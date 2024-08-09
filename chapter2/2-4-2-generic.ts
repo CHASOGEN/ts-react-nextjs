7b240c453de0d9eeb9c2bfefef7338c770e34c72
CHASOGEN <chasogen@gmail.com>
2024-08-09 23:10:19 +0900

    今日の分おわり！

---
+++ chapter2/2-4-2-generic.ts
@@ -0,0 +1,27 @@
+/*
+
+// T はクラス内で利用する仮の名前です
+class Queue<T> {
+    // 内部に T の型の配列を初期化する
+    private array: T:[] = []
+
+    // T の型の値を配列に追加します
+    push(item.T) {
+        this.array.push(item)
+    }
+
+    // T の型の配列最初の値を取り出します
+    pop(): T | undefined {
+        return this.array.shift()
+    }
+}
+
+const queue = new Queue<number>() // 数値型を扱うキューを生成します
+queue.push(111)
+queue.push(112)
+queue.push('hoge') // number 型ではないのでコンパイル時にエラーになります
+
+let str = 'fuga'
+str = queue.pop() // str は number 型ではないのでコンパイル時にエラーになります
+
+*/
---
+++ chapter2/2-4-5-never.ts
@@ -0,0 +1,41 @@
+// エラーが常に返るような関数で決して値が正常に返らない場合に never 型を指定します
+function error(message: string):never {
+    throw new Error(message)
+}
+
+function foo(x: string | number | number[]):boolean {
+    if (typeof x === 'string'){
+        return true
+    } else if (typeof x === 'number'){
+        return false
+    }
+
+    // never を利用することで明示的に値が返らないことをコンパイラに伝えることができます
+    // never を使用しないと TypeScript はコンパイルエラーになります
+    return error('Never happen')
+}
+
+// 将来的にも定数が追加される可能性のある enum 型を定義します
+enum pageType {
+    ViewProfile,
+    EditProfile,
+    ChangePassword,
+}
+
+const getTitleText = (type: pageType) => {
+    switch (type){
+        case pageType.ViewProfile:
+            return 'Setting'
+        case pageType.EditProfile:
+            return 'Edit Profile'
+        case pageType.ChangePassword:
+            return 'Change Password'
+        default:
+            // 決して起きないことをコンパイラに伝える never 型に代入を行います
+            // これによって仮に PageType の enum 型に定数が新規で使いされた際に
+                // コンパイル時にエラーが起きるためバグを未然に防ぐ対応を行うことができます
+        const wrongType: never = type
+        throw new Error('${wrongType} is not in PageType')
+    }
+}
+
