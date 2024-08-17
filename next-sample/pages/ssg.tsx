500b372e3aca1db888b38d06416f505728d99048
CHASOGEN <chasogen@gmail.com>
2024-08-17 09:46:43 +0900

    3.8.2 SSG によるページの実装

--- next-sample/pages/ssg.tsx
+++ next-sample/pages/ssg.tsx
@@ -7,4 +7,24 @@ import Head from 'next/head'
 type SSGProps = {}
 
 // SSG 向けのページを実装
-// NextPage は Next.js の
+// NextPage は Next.js の Pages 向けの型
+// NextPage<props> で props が入る Page であることを明示
+const SSG: NextPages<SSGProps> = () => {
+    return (
+        <div>
+            {/**Head コンポーネントで包むと、その要素は <head> タグに配置されます */}
+            <Head>
+                <title>Static Site</title>
+                <link rel="icon" href="/favicon.ico/">
+            </Head>
+            <main>
+                <p>
+                    このページは静的サイト生成によってビルド時に生成されたページです
+                </p>
+            </main>
+        </div>
+    )
+}
+
+// ページコンポーネントは export default でエクスポートする
+export default SSG
