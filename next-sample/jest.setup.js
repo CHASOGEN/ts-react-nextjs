5c252e3e2ab28370452a0d58103ac0e527879c69
CHASOGEN <chasogen@gmail.com>
2024-08-31 21:36:14 +0900

    4.4.4 非同期コンポーネントのユニットテスト

--- next-sample/jest.setup.js
+++ next-sample/jest.setup.js
@@ -1,1 +1,3 @@
-import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom'
+// import '@testing-library/jest-dom' がテキスト通りだったが、
+// @testing-library/jest-dom は存在しないため、配下全体とした
